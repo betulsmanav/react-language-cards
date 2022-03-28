d7fc8213d3ffd507257598c233ba06a55dcb0f6f
betul <betulsmanav@gmail.com>
2022-03-28 13:07:25 +0300

    first commit

---
+++ src/components/item/Item.js
@@ -0,0 +1,50 @@
+import { useState } from "react"; 
+import Card from "../card/Card";
+
+import "./Item.css";
+
+
+
+const Item = ({ card }) => {
+  const [showLogo, setShowLogo] = useState(true);
+  return (
+    <div className="card" onClick={()=>setShowLogo(!showLogo)}>
+      {showLogo ?(
+        <div>
+        <img className="card-logo" src={card.img} alt="logo" />
+        <h3 className="card-title">{card.name}</h3>
+      </div>
+      ) : (
+          <ul className="list">
+        {card.options.map((element, index) => {
+          return <li key={index}>{element}</li>
+        })}
+      </ul>)
+      }
+    </div>
+  )
+}
+
+
+// const Item = ({ card }) => {
+//   const [showLogo, setShowLogo] = useState(true);
+  
+//   return (
+//     <div className="card" onClick={()=>setShowLogo(!showLogo)}>
+//      {showLogo ? ( <div>
+//         <img className="card-logo" src={card.img} alt="logo" />
+//         <h3 className="card-title">{card.name}</h3>
+//       </div>
+//       ):(
+//       <ul className="list">
+//         {card.options.map((element, index) => {
+//           return <li key={index}>{element}</li>
+//         })}
+//       </ul>)
+//       }
+      
+//     </div>
+//   )
+// }
+
+export default Item;
